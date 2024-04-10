f1291616a3f1118eaa124ae47d1e79cb09c1820d
zoeyj <zoeyj8170@gmail.com>
2024-04-09 18:56:14 -0700

    rename the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Myfridge from "./Pages/Myfridge/Myfridge";
+import HomePage from "./Pages/HomePage/HomePage";
 import Recipe from "./Pages/Recipe/Recipe"
 import Sidebar from "./Components/Sidebar/Sidebar"
 import Header from "./Components/Header/Header"
@@ -13,12 +13,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter >
-      <Header/>
       <Routes >
-          <Route path="/home" element={<Home />} />
-          <Route path="/myfridge/:place" element={<Myfridge />} />
-          <Route path="/myfridge" element={<Myfridge />} />
-          {/* <Route path="/:name" element={<Myfridge />} /> */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/myfridge/:place" element={<HomePage />} />
+          <Route path="/myfridge" element={<HomePage />} />
+
           <Route path="/recipe/:recipeId" element={<Recipe />} />
       </Routes>
       </BrowserRouter>
