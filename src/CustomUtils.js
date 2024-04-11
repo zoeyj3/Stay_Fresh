a86c26dcf90484c288294bdc16268d1fc05577b5
zoeyj <zoeyj8170@gmail.com>
2024-04-11 10:25:51 -0700

    add CustomUtils for days calculate

--- src/CustomUtils.js
+++ src/CustomUtils.js
@@ -1,19 +1,27 @@
 export function getCurrentDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const day = String(now.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
 
-  export function getBestBeforeDate(preservingTime) {
-    const now = new Date();
-    const daysToAdd = parseInt(preservingTime, 10);
-    if (!isNaN(daysToAdd)) {
-      now.setDate(now.getDate() + daysToAdd);
-    }
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const day = String(now.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+export function getBestBeforeDate(preservingTime) {
+  const now = new Date();
+  const daysToAdd = parseInt(preservingTime, 10);
+  if (!isNaN(daysToAdd)) {
+    now.setDate(now.getDate() + daysToAdd);
+  }
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+export function getPreserveDayByBestBefore(bestBeforeDate) {
+  const bestBefore = new Date(bestBeforeDate);
+  const today = new Date();
+  const diffInTime = bestBefore.getTime() - today.getTime()
+  const diffInDays = Math.ceil(diffInTime / (1000 * 3600 * 24))
+  return diffInDays;
 }
