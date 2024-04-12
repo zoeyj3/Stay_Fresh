db8ef595afd1683e292d56cf7a6a4a8f88c0ce7f
zoeyj <zoeyj8170@gmail.com>
2024-04-11 19:15:24 -0700

    fix search bug

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,26 +1,44 @@
 import "./Search.scss";
+
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Search({setSearchingItem}) {
   
+  const [hidden,setHidden] = useState(true);
+  function handlehidden (){
+    setHidden(false);
+  }
+  function handleclear(){
+    setHidden(true);
+  }
+
   // function handleSearch(event) {
   //     event.preventDefault();
   //     console.log(event.target.elements[0].value)
   function handleInputSearch(event){
     event.preventDefault();
-
-    setSearchingItem(event.target.search.value);
-    console.log(event.target.search.value)
+    if(event.target.value){
+      setSearchingItem(event.target.value);
+      console.log(event.target.value)
+    }
   };
   // }
   return (
     
     <div className="search">
-      <form onSubmit={handleInputSearch}>
-        <input type="text" name="search" placeholder="Search.."></input>
-        <button type="submit">Search</button>
+      <form >
+        <button onClick={handlehidden} type="button"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        {!hidden && (
+          <>
+        <input type="text" name="search" placeholder="Search.." onChange={handleInputSearch}></input>
+      <button onClick={handleclear}> <FontAwesomeIcon icon={faXmark} /> </button>
+      </>)}
       </form>
     </div>
   );
