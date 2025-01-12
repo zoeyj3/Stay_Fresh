f822c4ea809e1afaa1e4c2a6588f10af59967711
Zoey Jiang <zoeyj8170@gmail.com>
2025-01-12 14:20:08 -0800

    update time

--- src/CustomUtils.js
+++ src/CustomUtils.js
@@ -1,11 +1,14 @@
 export const API_ADDRESS = "https://stayfresh-2d6eb1146b8f.herokuapp.com";
 
-export function getCurrentDate() {
+export function getCurrentDateTime() {
   const now = new Date();
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 export function getBestBeforeDate(preservingTime) {
