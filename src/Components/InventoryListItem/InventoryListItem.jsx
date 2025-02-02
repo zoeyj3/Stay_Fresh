7001a9ce616f3a8fbbd03d70de566080c9b9c8ba
Zoey Jiang <zoeyj8170@gmail.com>
2025-02-01 20:37:35 -0800

    Fix the bug that inventories disappear

--- src/Components/InventoryListItem/InventoryListItem.jsx
+++ src/Components/InventoryListItem/InventoryListItem.jsx
@@ -11,7 +11,7 @@ import * as CustomUtils from "../../CustomUtils.js";
 
 function InventoryListItem({ inventory, CheckboxChange, setUpdatedInventory }) {
   let expired = false;
-  let expirewarning = false;
+  let expireWarning = false;
   let remainTime;
 
   // filtering the item expired and going to expire
@@ -19,7 +19,7 @@ function InventoryListItem({ inventory, CheckboxChange, setUpdatedInventory }) {
     expired = true;
     remainTime = 0;
   } else if (inventory.days_to_expire < 4) {
-    expirewarning = true;
+    expireWarning = true;
     remainTime = inventory.days_to_expire;
   } else {
     remainTime = inventory.days_to_expire;
@@ -39,93 +39,87 @@ function InventoryListItem({ inventory, CheckboxChange, setUpdatedInventory }) {
     }
   };
 
-  if (inventory.days_to_expire < 0)
-    return (
-      <>
-        {inventory && (
-          <div className="listitem">
-            <input
-              type="checkbox"
-              className="listitem__checkbox"
-              id={inventory.name}
-              value={inventory.name}
-              name={inventory.id}
-              onChange={handleCheckbox}
-            />
-            <div className="listitem__contentwrapper">
-              <p className="listitem__name">{inventory.name} </p>
-              <div className="listitem__details">
-                <div className="listitem__details-wrapper">
-                  <FontAwesomeIcon
-                    className="listitem__icon"
-                    icon={faUtensils}
-                  />
-                  <p className="listitem__detailstext">
-                    {inventory.servings} left{" "}
-                  </p>
-                </div>
-                <div className="listitem__details-wrapper">
-                  {expirewarning && (
-                    <>
-                      <FontAwesomeIcon
-                        className="listitem__icon-red"
-                        icon={faCalendarXmark}
-                      />{" "}
-                      <p className="listitem__detailstext-red">
-                        {remainTime} day left
-                      </p>
-                    </>
-                  )}
-                  {expired && (
-                    <>
-                      <FontAwesomeIcon
-                        className="listitem__icon-red"
-                        icon={faCalendarXmark}
-                      />
-                      <p className="listitem__detailstext-red"> expired </p>
-                    </>
-                  )}
-                  {!expirewarning && !expired && (
-                    <>
-                      <FontAwesomeIcon
-                        className="listitem__icon"
-                        icon={faCalendarXmark}
-                      />
-                      <p className="listitem__detailstext">
-                        {inventory.best_before}
-                      </p>
-                    </>
-                  )}
-                </div>
-                <div className="listitem__details-wrapper-hidden">
-                  <FontAwesomeIcon
-                    className="listitem__icon"
-                    icon={faDoorOpen}
-                  />
-                  <p className="listitem__detailstext">
-                    {inventory.storing_place}
-                  </p>
-                </div>
+  // if (inventory.days_to_expire < 0)
+  return (
+    <>
+      {inventory && (
+        <div className="listitem">
+          <input
+            type="checkbox"
+            className="listitem__checkbox"
+            id={inventory.name}
+            value={inventory.name}
+            name={inventory.id}
+            onChange={handleCheckbox}
+          />
+          <div className="listitem__contentwrapper">
+            <p className="listitem__name">{inventory.name} </p>
+            <div className="listitem__details">
+              <div className="listitem__details-wrapper">
+                <FontAwesomeIcon className="listitem__icon" icon={faUtensils} />
+                <p className="listitem__detailstext">
+                  {inventory.servings} left{" "}
+                </p>
+              </div>
+              <div className="listitem__details-wrapper">
+                {expireWarning && (
+                  <>
+                    <FontAwesomeIcon
+                      className="listitem__icon-red"
+                      icon={faCalendarXmark}
+                    />{" "}
+                    <p className="listitem__detailstext-red">
+                      {remainTime} day left
+                    </p>
+                  </>
+                )}
+                {expired && (
+                  <>
+                    <FontAwesomeIcon
+                      className="listitem__icon-red"
+                      icon={faCalendarXmark}
+                    />
+                    <p className="listitem__detailstext-red"> expired </p>
+                  </>
+                )}
+                {!expireWarning && !expired && (
+                  <>
+                    <FontAwesomeIcon
+                      className="listitem__icon"
+                      icon={faCalendarXmark}
+                    />
+                    <p className="listitem__detailstext">
+                      {inventory.best_before}
+                    </p>
+                  </>
+                )}
+              </div>
+              <div className="listitem__details-wrapper-hidden">
+                <FontAwesomeIcon className="listitem__icon" icon={faDoorOpen} />
+                <p className="listitem__detailstext">
+                  {inventory.storing_place}
+                </p>
               </div>
             </div>
-            <div className="listitem__button-wrapper">
-              <EditInventory
-                inventoryId={inventory.id}
-                inventoryName={inventory.name}
-                inventoryServings={inventory.servings}
-                inventoryBestBeforeDate={inventory.best_before}
-                inventoryStoringPlace={inventory.storing_place}
-                setUpdatedInventory={setUpdatedInventory}
-              />
-              <FontAwesomeIcon
-                icon={faTrashCan}
-                className="listitem__trashicon"
-                onClick={() => handleDelete(inventory.id)}
-              />
-            </div>
           </div>
-        )}
-      </>
-    );
+          <div className="listitem__button-wrapper">
+            <EditInventory
+              inventoryId={inventory.id}
+              inventoryName={inventory.name}
+              inventoryServings={inventory.servings}
+              inventoryBestBeforeDate={inventory.best_before}
+              inventoryStoringPlace={inventory.storing_place}
+              setUpdatedInventory={setUpdatedInventory}
+            />
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              className="listitem__trashicon"
+              onClick={() => handleDelete(inventory.id)}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 export default InventoryListItem;
